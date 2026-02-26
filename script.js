8a3c0d9f90b44d49b593b7b9960442c67a30b13f
nada-precious <92520197+nada-precious@users.noreply.github.com>
2026-02-27 00:41:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -286,7 +286,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const timeLabel = document.getElementById("timeLabel");
 
     // Show expired look immediately
-    countdownEl.innerText = "💙 Today is over  precious";
+    countdownEl.innerText = " Today is over  precious 💙";
 
     loveMessageEl.innerText =
         "Remember, our love is bigger than one day!";
